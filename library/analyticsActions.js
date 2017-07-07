875d5bb0a824b8d360f68aa04cf4ad8a53697aa3
Yunus Durmus <yunus.durmus@akinon.com>
2017-07-07 14:32:26 +0300

    fixed actions list

--- library/analyticsActions.js
+++ library/analyticsActions.js
@@ -13,6 +13,14 @@ module.exports = {
                     analytics.page(data);
                 }
             },
+
+            //Browsing
+            {
+                type: 'Products Searched',
+                push: function(data) {
+                  analytics.track('Products Searched', data);
+                }
+            },
             {
                 type: 'Product List Viewed',
                 push: function(data) {
@@ -20,65 +28,177 @@ module.exports = {
                 }
             },
             {
-                type: 'Product Viewed',
+                type: 'Product List Filtered',
                 push: function(data) {
-                  analytics.track('Product Viewed' ,data);
+                  analytics.track('Product List Filtered', data);
+                }
+            },
+
+            //Promotions
+            {
+                type: 'Promotion Viewed',
+                push: function(data) {
+                  analytics.track('Product List Viewed', data);
+                }
+            },
+            {
+                type: 'Promotion Clicked',
+                push: function(data) {
+                  analytics.track('Product List Filtered', data);
+                }
+            },
+            
+            //Ordering
+            {
+                type: 'Product Clicked',
+                push: function(data) {
+                  analytics.track('Product Clicked', data);
                 }
             },
             {
                 type: 'Product Viewed',
                 push: function(data) {
-                    analytics.track('Product Viewed', data);
-                }
-            },
-            {
-                type: 'Product Viewed',
-                push: function(data) {
-                    analytics.track('Product Viewed', data);
-                }
-            },
-            {
-                type: 'Cart Viewed',
-                push: function(data) {
-                    analytics.track('Cart Viewed', data);
+                  analytics.track('Product Viewed', data);
                 }
             },
             {
                 type: 'Product Added',
                 push: function(data) {
-                    analytics.track('Product Added', data);
+                  analytics.track('Product Added', data);
                 }
             },
             {
                 type: 'Product Removed',
                 push: function(data) {
-                    analytics.track('Product Removed', data);
+                  analytics.track('Product Removed', data);
                 }
             },
             {
-                type: 'Checkout Step Viewed',
+                type: 'Cart Viewed',
                 push: function(data) {
-                    analytics.track('Checkout Step Viewed', data);
+                  analytics.track('Cart Viewed', data);
                 }
             },
             {
                 type: 'Checkout Started',
                 push: function(data) {
-                    analytics.track('Checkout Started', data);
+                  analytics.track('Checkout Started', data);
+                }
+            },
+            {
+                type: 'Checkout Step Viewed',
+                push: function(data) {
+                  analytics.track('Checkout Step Viewed', data);
                 }
             },
             {
                 type: 'Checkout Step Completed',
                 push: function(data) {
-                    analytics.track('Checkout Step Completed', data);
+                  analytics.track('Checkout Step Completed', data);
+                }
+            },
+            {
+                type: 'Payment Info Entered',
+                push: function(data) {
+                  analytics.track('Payment Info Entered', data);
                 }
             },
             {
                 type: 'Order Completed',
                 push: function(data) {
-                    analytics.track('Order Completed', data);
+                  analytics.track('Order Completed', data);
+                }
+            },
+            {
+                type: 'Order Updated',
+                push: function(data) {
+                  analytics.track('Order Updated', data);
+                }
+            },
+            {
+                type: 'Order Refunded',
+                push: function(data) {
+                  analytics.track('Order Refunded', data);
+                }
+            },
+            {
+                type: 'Order Cancelled',
+                push: function(data) {
+                  analytics.track('Order Cancelled', data);
+                }
+            },
+
+
+            
+            //Coupons
+            {
+                type: 'Coupon Entered',
+                push: function(data) {
+                  analytics.track('Coupon Entered', data);
+                }
+            },
+            {
+                type: 'Coupon Applied',
+                push: function(data) {
+                  analytics.track('Coupon Applied', data);
+                }
+            },
+            {
+                type: 'Coupon Denied',
+                push: function(data) {
+                  analytics.track('Coupon Denied', data);
+                }
+            },
+            {
+                type: 'Coupon Removed',
+                push: function(data) {
+                  analytics.track('Coupon Removed', data);
+                }
+            },
+
+
+            //Wishlisting
+            {
+                type: 'Product Added to Wishlist',
+                push: function(data) {
+                  analytics.track('Product Added to Wishlist', data);
+                }
+            },
+            {
+                type: 'Product Removed from Wishlist',
+                push: function(data) {
+                  analytics.track('Product Removed from Wishlist', data);
+                }
+            },
+            {
+                type: 'Wishlist Product Added to Cart',
+                push: function(data) {
+                  analytics.track('Wishlist Product Added to Cart', data);
+                }
+            },
+
+            //Sharing
+            {
+                type: 'Product Shared',
+                push: function(data) {
+                  analytics.track('Product Shared', data);
+                }
+            },
+            {
+                type: 'Cart Shared',
+                push: function(data) {
+                  analytics.track('Cart Shared', data);
+                }
+            },
+
+            //Reviewing
+            {
+                type: 'Product Reviewed',
+                push: function(data) {
+                  analytics.track('Product Reviewed', data);
                 }
             }
+
         ]
     }
 }
